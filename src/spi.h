b4db372d16f3e473e46df7112040d87bd57b88cd
Ben Firner <ben@owlplatform.com>
2015-05-28 17:05:19 -0400

    Toggling correct reset line on the Pi B+

--- src/spi.h
+++ src/spi.h
@@ -30,7 +30,7 @@
 #define MISO RPI_GPIO_P1_21
 #define SCLK RPI_GPIO_P1_23
 //Reset pin 5, GPIO3
-#define RESET RPI_GPIO_P1_05
+#define RESET RPI_BPLUS_GPIO_J8_05     
 //#define RESET RPI_BPLUS_GPIO_J8_31
 
 #define REQ_DROPPED 0xFC
